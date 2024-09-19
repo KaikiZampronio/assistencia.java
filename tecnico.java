85c7e540745c48185e48043ce3bf9530715c9016
Kaiki Zampronio <160637193+KaikiZampronio@users.noreply.github.com>
2024-09-19 19:56:42 -0300

    Create tecnico.java

---
+++ tecnico.java
@@ -0,0 +1,13 @@
+class Tecnico {
+    private String nome;
+    private String especialidade;
+
+    public Tecnico(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+}
